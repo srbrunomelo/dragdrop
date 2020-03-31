068031f1dd7835e9d8cb8f8ea205953732d8555f
Bruno Melo <srbrunomelo@gmail.com>
2020-03-31 13:38:42 -0300

    aplicação 1.0 finalizada

---
+++ src/Services/api.js
@@ -0,0 +1,97 @@
+export function loadLists() {
+    return [
+      { 
+        title: 'Tarefas', 
+        creatable: true,
+        cards: [
+          {
+            id: 1,
+            content: 'Estudar NodeJS',
+            labels: ['#7159c1'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+          {
+            id: 2,
+            content: 'Criar lorem para o lorem ensinando a lorem a mody do lorem',
+            labels: ['#7159c1'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+          {
+            id: 3,
+            content: 'É importante questionar o quanto a crescente influência da mídia afeta positivamente',
+            labels: ['#7159c1'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+          {
+            id: 4,
+            content: 'lorem grabs os  ReactJS"',
+            labels: ['#54e1f7'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+          {
+            id: 5,
+            content: 'correta previsão do sistema de formação de quadros que corresponde às necessidades.',
+            labels: ['#54e1f7'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+        ]
+      },
+      { 
+        title: 'Fazendo', 
+        creatable: false,
+        cards: [
+          {
+            id: 6,
+            content: 'Recriando clone do Pipefy',
+            labels: [],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          }
+        ]
+      },
+      { 
+        title: 'Pausado', 
+        creatable: false,
+        cards: [
+          {
+            id: 7,
+            content: 'Podemos já vislumbrar o modo pelo qual o consenso sobre a necessidade de',
+            labels: ['#7159c1'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+          {
+            id: 8,
+            content: 'qualificação desafia a capacidade de equalização do impacto na agilidade decisória.',
+            labels: ['#54e1f7'],
+            user: 'https://d22iebrrkdwkpr.cloudfront.net/avatars/imoth/bruno-melo/a61238141e4f0719857ed2ec9ecd0bbbregular.jpg'
+          },
+          {
+            id: 9,
+            content: 'nossa atividade maximiza as possibilidades por conta do remanejamento dos quadros funcionais.',
+            labels: [],
+          }
+        ]
+      },
+      { 
+        title: 'Concluído', 
+        creatable: false,
+        done: true,
+        cards: [
+          {
+            id: 10,
+            content: 'Gostaria de enfatizar que a contínua expansão de teste',
+            labels: [],
+          },
+          {
+            id: 12,
+            content: 'atuação afeta positivamente a correta previsão dos modos de operação convencionais.',
+            labels: ['#54e1f7'],
+          },
+          {
+            id: 13,
+            content: 'O incentivo ao avanço tecnológico, assim como o desenvolvimento contínuo de distintas formas d',
+            labels: ['#7159c1'],
+          }
+        ]
+      },
+    ];
+  }
